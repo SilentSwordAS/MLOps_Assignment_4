d7c95f2c2c599068c6a8df9d7861c9701c605949
SilentSwordAS <anuraag2002shukla@gmail.com>
2025-10-19 19:03:40 +0000

    surely 4

--- test_evaluation.py
+++ test_evaluation.py
@@ -3,18 +3,23 @@ import joblib
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+
 
 data = pd.read_csv('augmented_train_v2.csv')
 X_train, X_test = train_test_split(data, test_size=0.2, random_state=42, stratify=data['species'])
+le = LabelEncoder()
+
+y_train = le.fit_transform(X_train['species'])
 
 def test_model_exists():
-    model = joblib.load('augmented_train_model_v2.joblib')
+    model = joblib.load('augmented_train_v2_model.joblib')
     assert model
 
 def test_model_accuracy():
-    model = joblib.load('augmented_train_model_v2.joblib')
+    model = joblib.load('augmented_train_v2_model.joblib')
     y_pred = model.predict(X_test.drop(['species'], axis=1))
-    model_score = accuracy_score(X_test['species'], y_pred)
+    model_score = accuracy_score(le.transform(X_test['species']), y_pred)
     assert model_score > 0.9
 
 def test_data_header_validation():
