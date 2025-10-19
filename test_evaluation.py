4f86d6e624eb137a49feb8dadedb9e91e8b30cb6
SilentSwordAS <anuraag2002shukla@gmail.com>
2025-10-19 15:51:22 +0000

    Checking commit

---
+++ test_evaluation.py
@@ -0,0 +1,27 @@
+import pytest
+import joblib
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv('augmented_train_v2.csv')
+X_train, X_test = train_test_split(data, test_size=0.2, random_state=42, stratify=data['species'])
+
+def test_model_exists():
+    model = joblib.load('augmented_train_model_v2.joblib')
+    assert model
+
+def test_model_accuracy():
+    model = joblib.load('augmented_train_model_v2.joblib')
+    y_pred = model.predict(X_test.drop(['species'], axis=1))
+    model_score = accuracy_score(X_test['species'], y_pred)
+    assert model_score > 0.9
+
+def test_data_header_validation():
+    data_columns = list(data.columns.values)
+    assert len(data_columns) == 5
+
+def test_data_number_validation():
+    assert len(data) == 300
+
+
